d586907b37f4e89b7398b81a4c51b4136a1f5984
Belchior <belchiorbenilson@gmail.com>
2024-02-19 18:04:24 +0100

    atualizei o controller das frases

--- controllers/fraseController.js
+++ controllers/fraseController.js
@@ -1,13 +1,30 @@
 const fraseModel = require('../models/frase');
 
+function formatarPalavra(palavra){
+    const palavraFormatada = palavra.slice(0,1).toUpperCase() + palavra.slice(1,palavra.length).toLowerCase();
+    return palavraFormatada
+}
+
 const fraseController = {
+
     salvarFrase: async(req, res) => {
         try{
-            const novaFrase = await fraseModel.create(req.body);
+            const { frase, traducao} = req.body
 
-            res.status(201).json({
-                'msg': 'Nova frase adicionada com sucesso'
-            })
+            if(!frase || !traducao){
+                res.status(400).json({
+                    'msg': 'Preencha todos os campos'
+                })
+            }else{
+                await fraseModel.create({
+                    frase: formatarPalavra(frase),
+                    traducao: formatarPalavra(traducao)
+                });
+    
+                res.status(201).json({
+                    'msg': 'Nova frase adicionada com sucesso'
+                })
+            }
         }catch(error){
             res.status(400).json({
                 'msg': error
@@ -18,18 +35,18 @@ const fraseController = {
         const {frase} = req.body
 
         try{
-            const traducao = await fraseModel.findAll({
+            const traducao = await fraseModel.findOne({
                 attributes: ['traducao'],
                 where: {
                     frase,
                 }
             })
 
-            if(traducao.length == 0){
+            if(!traducao){
                 res
                 .status(404)
                 .json({
-                    'msg': 'Frase não encontrdada'
+                    'msg': 'Tradução não encontrdada'
                 })
             }else{
                 res.send(traducao)
